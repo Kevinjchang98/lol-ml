cedc6fe04cae7c95c9a59b52e0fc12c508763e63
Kevin Chang <kevinjchang98@gmail.com>
2020-08-16 13:57:57 -0700

    refactor minimal working code

--- main.py
+++ main.py
@@ -2,35 +2,54 @@ import requests
 import apiKey #apiKey.py with apiKey = ''
 import time
 import pandas as pd
+import csv
 
 #TODO: Deal with possible errors in get requests
 
-# Initialize returnList which is final list to be saved
-returnList = []
+def main():
+    matchList = getMatchList('dyrus')
+    getMatchDetails(matchList)
 
-# Initial request for basic info
-response = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/dyrus?api_key=' + apiKey.apiKey, timeout = 5)
-data = response.json()
-
-# Set account Id needed to get match lists
-accountId = data['accountId']
-
-# Get match list
-response = requests.get('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + accountId + '?api_key=' + apiKey.apiKey, timeout = 5)
-data = response.json()
-
-# For each match in the match list, append all 10 player's data to returnList object
-for match in data['matches'][:5]:
-    print('Match ID: ' + str(match['gameId']))
-    response = requests.get('https://na1.api.riotgames.com/lol/match/v4/matches/' + str(match['gameId']) + '?api_key=' + apiKey.apiKey, timeout = 5)
-    time.sleep(2) # 2 second delay for rate limit
-
+def getMatchList(name):
+    # Initial request for basic info
+    response = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + name + '?api_key=' + apiKey.apiKey, timeout = 5)
     data = response.json()
-    for player in data['participants']:
-        # print(player['participantId'])
-        returnList.append(player['stats'])
 
-# Save returnList as test.txt
-f = open('test.txt', 'w')
-f.write(str(returnList))
-f.close()
+    # Set account Id needed to get match lists
+    accountId = data['accountId']
+
+    # Get match list
+    response = requests.get('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + accountId + '?api_key=' + apiKey.apiKey, timeout = 5)
+    matchList = response.json()
+
+    return matchList
+
+def getMatchDetails(matchList):
+    # Initialize returnList which is final list to be saved
+    returnList = []
+
+    # For each match in the match list, append all 10 player's data to returnList object
+    for match in matchList['matches']:
+        print('Match ID: ' + str(match['gameId']))
+        response = requests.get('https://na1.api.riotgames.com/lol/match/v4/matches/' + str(match['gameId']) + '?api_key=' + apiKey.apiKey, timeout = 5)
+        time.sleep(5) # 5 second delay for rate limit
+
+        data = response.json()
+        for player in data['participants']:
+            # print(player['participantId'])
+            returnList.append(player['stats'])
+
+        # Convert list of dict to pandas dataframe
+        returnDf = pd.DataFrame(returnList)
+
+        # Select only following columns
+        headerList = ['win', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'perk0',	'perk0Var1',	'perk0Var2',	'perk0Var3',	'perk1',	'perk1Var1',	'perk1Var2',	'perk1Var3',	'perk2',	'perk2Var1',	'perk2Var2',	'perk2Var3',	'perk3',	'perk3Var1',	'perk3Var2',	'perk3Var3',	'perk4',	'perk4Var1',	'perk4Var2',	'perk4Var3',	'perk5',	'perk5Var1',	'perk5Var2',	'perk5Var3',	'perkPrimaryStyle',	'perkSubStyle',	'statPerk0',	'statPerk1',	'statPerk2'
+        ]
+        returnDf = returnDf[headerList]
+
+        # Append to data.csv
+        with open('data.csv', 'a') as f:
+            returnDf.to_csv(f, index=False, header=False)
+
+if __name__ == "__main__":
+    main()
