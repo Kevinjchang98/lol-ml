926fb816194f16e625b6ca75b9d746960c3e3135
Kevin Chang <kevinjchang98@gmail.com>
2020-09-16 15:54:55 -0700

    Add championId as a recorded variable

--- main.py
+++ main.py
@@ -60,7 +60,9 @@ def getMatchDetails(matchList):
             data = response.json()
             for player in data['participants']:
                 # print(player['participantId'])
-                returnList.append(player['stats'])
+                currentMatchDict = player['stats']
+                currentMatchDict['championId'] = player['championId']
+                returnList.append(currentMatchDict)
 
             for player in data['participantIdentities']:
                 nameList.append(player['player']['summonerName'])
@@ -69,7 +71,7 @@ def getMatchDetails(matchList):
             returnDf = pd.DataFrame(returnList)
 
             # Select only following columns
-            headerList = ['win', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'perk0',	'perk0Var1',	'perk0Var2',	'perk0Var3',	'perk1',	'perk1Var1',	'perk1Var2',	'perk1Var3',	'perk2',	'perk2Var1',	'perk2Var2',	'perk2Var3',	'perk3',	'perk3Var1',	'perk3Var2',	'perk3Var3',	'perk4',	'perk4Var1',	'perk4Var2',	'perk4Var3',	'perk5',	'perk5Var1',	'perk5Var2',	'perk5Var3',	'perkPrimaryStyle',	'perkSubStyle',	'statPerk0',	'statPerk1',	'statPerk2'
+            headerList = ['win', 'item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'perk0',	'perk0Var1',	'perk0Var2',	'perk0Var3',	'perk1',	'perk1Var1',	'perk1Var2',	'perk1Var3',	'perk2',	'perk2Var1',	'perk2Var2',	'perk2Var3',	'perk3',	'perk3Var1',	'perk3Var2',	'perk3Var3',	'perk4',	'perk4Var1',	'perk4Var2',	'perk4Var3',	'perk5',	'perk5Var1',	'perk5Var2',	'perk5Var3',	'perkPrimaryStyle',	'perkSubStyle',	'statPerk0',	'statPerk1',	'statPerk2', 'championId'
             ]
             returnDf = returnDf[headerList]
 
